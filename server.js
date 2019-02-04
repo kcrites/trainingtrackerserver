926b6cf6ccf9154b62023f2169613ec970342dc8
Ken Crites <kenrcrites@gmail.com>
2019-02-04 23:19:38 +0100

    Initial stats functionality

--- server.js
+++ server.js
@@ -13,20 +13,28 @@ app.use(bodyParser.json());
 
 const database = {
 	users: [ {
-	id: '123',
-	name: 'Ken',
-	email: 'ken@gmail.com',
-	password: 'dog',
-	joined: new Date()
-},
-	{
-	id:'124',
-	name: 'Jen',
-	email: 'jen@gmail.com',
-	password: 'cat',
-	joined: new Date()
-	}
-	]
+			id: '123',
+			name: 'Ken',
+			email: 'ken@gmail.com',
+			password: 'dog',
+			joined: new Date()
+			},
+			{
+			id:'124',
+			name: 'Jen',
+			email: 'jen@gmail.com',
+			password: 'cat',
+			joined: new Date()
+			}
+			],
+	stats: [{
+		id:'123',
+		weight: 93,
+		height:175,
+		bmi: 30.2,
+		workout: '12/12/1212',
+
+	}]
 }
 
 app.get('/', (req, res) => { res.send('It is working'); })
@@ -52,6 +60,20 @@ const { email, name, password} = req.body
 		joined: new Date()
 	});
 	res.json(database.users[database.users.length-1]);
+})
+
+app.post('/stats', (req, res) => {
+	const {id, weight, height, bmi} = req.body
+		database.stats.push({
+			id:id,
+			weight: weight,
+			height: height,
+			bmi: bmi,
+			workout: new Date()
+
+		});
+		res.json(database.stats[database.stats.length-1]);
+		console.log(database.stats)
 })
 //app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 
