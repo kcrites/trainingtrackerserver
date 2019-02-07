daffc02bd9141c248d246dbde5c573e109f31bd1
Ken Crites <kenrcrites@gmail.com>
2019-02-08 00:20:54 +0100

    adding encryption

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 //Controllers
 /*const register = require('./controllers/register');
@@ -10,23 +12,23 @@ const image = require('./controllers/image');*/
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
 	users: [ {
-			id: '123',
-			name: 'Ken',
-			email: 'ken@gmail.com',
-			password: 'dog',
-			joined: new Date()
-			},
-			{
-			id:'124',
-			name: 'Jen',
-			email: 'jen@gmail.com',
-			password: 'cat',
-			joined: new Date()
-			}
-			],
+		id: '123',
+		name: 'Ken',
+		email: 'ken@gmail.com',
+		password: 'dog',
+		joined: new Date()
+		},
+		{
+		id:'124',
+		name: 'Jen',
+		email: 'jen@gmail.com',
+		password: 'cat',
+		joined: new Date()
+		}],
 	stats: [{
 		id:'123',
 		weight: 93,
@@ -34,10 +36,15 @@ const database = {
 		bmi: 30.2,
 		workout: '12/12/1212',
 
-	}]
+		}],
+	login: [{
+		id: '987',
+		hash: '$2a$10$tWppzxMhW1Rp9wVOzQbSU.OzKq.PaY02/GJ64ZmhUInsvhkKUFuF6',
+		email: 'ken@gmail.com'
+		}]
 }
 
-app.get('/', (req, res) => { res.send('It is working'); })
+app.get('/', (req, res) => { res.send(database.users); })
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
@@ -56,7 +63,7 @@ app.get('/profile/:id', (req, res) => {
 //app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/signin', (req, res) => {
 	if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-		res.json('Success');
+		res.json(database.users[0]);
 	}
 	else {
 		res.status(400).json('Error logging in');
@@ -65,11 +72,14 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
 const { email, name, password} = req.body
+bcrypt.hash(password, null, null, function(err, hash) {
+	console.log(hash);
+});
 	database.users.push({
 		id: '125',
 		name: name,
 		email: email,
-		password: password,
+		
 		entries: 0,
 		joined: new Date()
 	});
